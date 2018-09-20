30304d775729b68e82a5dd3da370a8165ae37904
DorianPillant <dorianpillant@gmail.com>
2018-09-20 09:56:57 +0200

    connexion and deconnexion user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,13 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends Controller
 {
    /**
      * @Route("/inscription", name="security_registration")
      */ 
-   public function registration(Request $request, ObjectManager $manager){
+   public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder){
 
         $user = new User();
         //on relit les champs du formulaire au champ de l'utilisateur
@@ -24,8 +25,14 @@ class SecurityController extends Controller
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($hash);
+
             $manager->persist($user);
             $manager->flush();
+
+            return $this->redirectToRoute('security_login');
 
         }
 
@@ -36,4 +43,18 @@ class SecurityController extends Controller
 
 
    }
+
+   /**
+   * @Route("/connexion", name="security_login")
+   */
+   public function login(){
+       return $this->render('security/login.html.twig');
+   }
+
+   /**
+   * @Route("/deconnexion", name="security_logout")
+   */
+   public function logout(){}
+    //ici il me faut juste une route
+   
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,15 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"}, message="L'email que vous avez utilisé est déjà utilisé")
+ * @UniqueEntity(fields={"username"}, message="L'username que vous avez utilisé est déjà utilisé")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -26,16 +30,16 @@ class User
      * @ORM\Column(type="string", length=255)
      */
     private $username;
+    
     //EqualTo Valide le fait que le champ sois similaire à quelque chose d'autre et je veux que cette propriété $password sois égal à une autre propriéter de mon objet
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\Length(min="8", minMessage="Votre mot de passe doit faire minimum 8 caractères")
-     * @Assert\EqualTo(propertyPath="confirm_password, message="Votre mot de passe n'est pas identique")
      */
     private $password;
 
     /**
-     * @Assert\EqualTo(propertyPath="password, message="Votre mot de passe n'est pas identique")
+     * @Assert\EqualTo(propertyPath="password", message="Votre mot de passe n'est pas identique")
      */
     public $confirm_password;
 
@@ -79,4 +83,13 @@ class User
 
         return $this;
     }
+
+    public function eraseCredentials() {} 
+
+    public function getSalt() {}
+
+    public function getRoles() {
+        return ['ROLE_USER']; 
+    }    
+    
 }
